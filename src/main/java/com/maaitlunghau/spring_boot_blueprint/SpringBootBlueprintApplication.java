e81b7ebe187101c4f9060921eef05196394f85ff
maaitlunghau <chunhau.py@gmail.com>
2026-08-18 17:01:01 +0700

    chore: intial spring boot project

---
+++ src/main/java/com/maaitlunghau/spring_boot_blueprint/SpringBootBlueprintApplication.java
@@ -0,0 +1,13 @@
+package com.maaitlunghau.spring_boot_blueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlueprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlueprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maaitlunghau/spring_boot_blueprint/SpringBootBlueprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maaitlunghau.spring_boot_blueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlueprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
