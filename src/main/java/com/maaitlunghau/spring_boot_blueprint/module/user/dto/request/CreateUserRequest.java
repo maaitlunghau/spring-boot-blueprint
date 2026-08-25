132485d45e4a6a0931955cf5ab86c21b52773f7d
maaitlunghau <chunhau.py@gmail.com>
2026-08-25 16:53:13 +0700

    feat: add CreateUserRequest DTO

---
+++ src/main/java/com/maaitlunghau/spring_boot_blueprint/module/user/dto/request/CreateUserRequest.java
@@ -0,0 +1,21 @@
+package com.maaitlunghau.spring_boot_blueprint.module.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserRequest(
+    @NotBlank(message = "Full name is required")
+    String fullName,
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email format should be valid")
+    String email,
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    String password,
+
+    @NotBlank(message = "Role is required")
+    String role
+) {}
