efb24ff51b58b002b7ca72f54c2b5cadf1a4abdc
maaitlunghau <chunhau.py@gmail.com>
2026-08-29 18:12:32 +0700

    feat: add notification exchange/queue/DLQ topology

---
+++ src/main/java/com/maaitlunghau/spring_boot_blueprint/config/RabbitMQConfig.java
@@ -0,0 +1,73 @@
+package com.maaitlunghau.spring_boot_blueprint.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String NOTIFICATION_EXCHANGE = "notification.exchange";
+    public static final String NOTIFICATION_DLX = "notification.dlx";
+    public static final String NOTIFICATION_DLQ = "notification.dlq";
+
+    public static final String USER_BAN_QUEUE = "user.ban.notification.queue";
+    public static final String USER_UNBAN_QUEUE = "user.unban.notification.queue";
+
+    public static final String USER_BANNED_ROUTING_KEY = "user.banned";
+    public static final String USER_UNBANNED_ROUTING_KEY = "user.unbanned";
+
+    @Bean
+    public TopicExchange notificationExchange() {
+        return new TopicExchange(NOTIFICATION_EXCHANGE);
+    }
+
+    @Bean
+    public TopicExchange notificationDeadLetterExchange() {
+        return new TopicExchange(NOTIFICATION_DLX);
+    }
+
+    @Bean
+    public Queue notificationDeadLetterQueue() {
+        return QueueBuilder.durable(NOTIFICATION_DLQ).build();
+    }
+
+    @Bean
+    public Binding notificationDeadLetterBinding() {
+        return BindingBuilder.bind(notificationDeadLetterQueue()).to(notificationDeadLetterExchange()).with("#");
+    }
+
+    @Bean
+    public Queue userBanNotificationQueue() {
+        return QueueBuilder.durable(USER_BAN_QUEUE)
+            .withArgument("x-dead-letter-exchange", NOTIFICATION_DLX)
+            .build();
+    }
+
+    @Bean
+    public Binding userBanBinding() {
+        return BindingBuilder.bind(userBanNotificationQueue()).to(notificationExchange()).with(USER_BANNED_ROUTING_KEY);
+    }
+
+    @Bean
+    public Queue userUnbanNotificationQueue() {
+        return QueueBuilder.durable(USER_UNBAN_QUEUE)
+            .withArgument("x-dead-letter-exchange", NOTIFICATION_DLX)
+            .build();
+    }
+
+    @Bean
+    public Binding userUnbanBinding() {
+        return BindingBuilder.bind(userUnbanNotificationQueue()).to(notificationExchange()).with(USER_UNBANNED_ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
